35f0924dea5ff53a83bfa896229e26a86592a926
Nikita <nik.petrenko97@gmail.com>
2022-06-27 17:46:28 +0300

    trying https request

--- src/redux/actions/getDataAction.js
+++ src/redux/actions/getDataAction.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const axiosData = (city, lang) => async (dispatch) => {
     try {
-        const data = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${city}&lang=${lang}&appid=fcd7780b300a0d0160ee4aef2084c4a8`);
+        const data = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${city}&lang=${lang}&appid=fcd7780b300a0d0160ee4aef2084c4a8`);
 
         dispatch(getData(data))
     }
